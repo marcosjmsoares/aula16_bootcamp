257969338486c51f0b5fd029cb300631cb9d6fe7
Marcos Soares <marcosjmsoares@gmail.com>
2025-09-27 22:28:06 -0300

    adicionado arquivos main_create e main_read

---
+++ main_read.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlalchemy import text
+
+engine = create_engine("sqlite:///database.db", echo=True)
+
+with Session(engine) as session:
+    statement = text("select * from hero;")
+
+    results = session.exec(statement)
